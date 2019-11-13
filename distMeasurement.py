08c32ad52b9d5e9499fc882ffaa46d4e8202d1a7
AnonMuk <zubair.mukhi@gmail.com>
2019-11-12 21:27:10 -0500

    Creating script file

---
+++ distMeasurement.py
@@ -0,0 +1,6 @@
+import struct
+
+#unused port 33434
+
+msg = "measurement for class project; please direct inquiries to student zxm132@case.edu or professor mxr136@case.edu"
+payload = bytes(msg + )
